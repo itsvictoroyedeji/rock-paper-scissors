fc5ef9a0bb413479dff92aab4dda021daece6653
Victor chromebook <175169337+itsvictoroyedeji@users.noreply.github.com>
2024-10-10 11:51:28 -0400

    feat: Complete logic to play 5 rounds of r-p-scissors
    This commit adds code to the playGame function, along
    with the loop and variables moved around to achieve
    gameplay.
    
    The commit also adds optional logic that ends the
    game if human selection is undefined.

--- rps.js
+++ rps.js
@@ -45,89 +45,111 @@ function getHumanChoice() {
     }
 } 
 
-//create variable humanScore and assign value 0
-let humanScore = 0;
+// Declare global variables humanSelection and computerSelection to use in playGame() function
+let humanSelection;
+let computerSelection;
 
-//create variable computerScore and assign value 0
-let computerScore = 0;
+// Call playGame() function!
+playGame();
 
-// Get values from human selection and computer selection
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
-
-// Display human and computer selections
-console.log("Human Selection: " + humanSelection);
-console.log("Computer score: " + computerSelection);
-
-playRound(humanSelection, computerSelection)
-
-function playRound(humanChoice, computerChoice) {
-  // condition: if humanChoice equals undefined
-  if (humanChoice == undefined) {
-    // console.log: "Game didn't play. Try again"
-    console.log("Game didn't play. Try again");
-  // else if: if humanChoice equals rock and computerChoice equals paper
-  } else if ((humanChoice === "rock") && (computerChoice === "paper")) {
-    // console log: "You lose! Paper beats Rock."
-    console.log("You lose! Paper beats Rock");
-    // increment computerScore variable by one
-    computerScore++;
-  
-  // else if: humanChoice equals rock and computerChoice equals scissors
-  } else if ((humanChoice === "rock") && (computerChoice === "scissors")) {
-    // console log: "You win! Rock beats Scissors."
-    console.log("You win! Rock beats Scissors.")
-    // increment humanScore variable by one
-    humanScore++;
-
-  // else if: humanChoice equals paper and computerChoice equals scissors
-  } else if ((humanChoice === "paper") && (computerChoice === "scissors")) {
-    // console log: "You lose! Scissors beats Paper."
-    console.log("You lose! Scissors beats Paper.")
-    // increment computerScore variable by one
-    computerScore++;
-  // else if: humanChoice equals paper and computerChoice equals rock
-  } else if ((humanChoice === "paper") && (computerChoice === "rock")) {
-    // console log: "You win! Paper beats Rock."
-    console.log("You win! Paper beats Rock.")
-    // increment humanScore variable by one
-    humanScore++;
-
-  // else if: humanChoice equals scissors and computerChoice equals rock
-  } else if ((humanChoice === "scissors") && (computerChoice === "rock")) {
-    // console log: "You lose! Rock beats Scissors."
-    console.log("You lose! Rock beats Scissors.");
-    // increment computerScore variable by one
-    computerScore++;
-
-  // else if: humanChoice equals scissors and computerChoice equals paper
-  } else if ((humanChoice === "scissors") && (computerChoice === "paper")) {
-    // console log: "You win! Scissors beats Paper."
-    console.log("You win! Scissors beats Paper.");
-    // increment humanScore variable by one
-    humanScore++;
-
-  // else: console log: "It's a tie";
-  } else {
-    console.log("It's a tie.");
-  }
-}
-
-console.log("Human score: " + humanScore);
-console.log("Computer score: " + computerScore);
-
+// playGame function is declared here
 function playGame() {
-  // create variable humanScore and assign value 0
+  //create variable humanScore and assign value 0
+  let humanScore = 0;
 
-  // create variable computerScore and assign value 0
+  //create variable computerScore and assign value 0
+  let computerScore = 0;
+
+  // declare playRound function to configure single round of play
+  function playRound(humanChoice, computerChoice) {
+    // condition: if humanChoice equals undefined
+    if (humanChoice == undefined) {
+      // console.log: "Game didn't play. Try again"
+      console.log("Game didn't play. Try again");
+    // else if: if humanChoice equals rock and computerChoice equals paper
+    } else if ((humanChoice === "rock") && (computerChoice === "paper")) {
+      // console log: "You lose! Paper beats Rock."
+      console.log("You lose! Paper beats Rock");
+      // increment computerScore variable by one
+      computerScore++;
+    
+    // else if: humanChoice equals rock and computerChoice equals scissors
+    } else if ((humanChoice === "rock") && (computerChoice === "scissors")) {
+      // console log: "You win! Rock beats Scissors."
+      console.log("You win! Rock beats Scissors.")
+      // increment humanScore variable by one
+      humanScore++;
+  
+    // else if: humanChoice equals paper and computerChoice equals scissors
+    } else if ((humanChoice === "paper") && (computerChoice === "scissors")) {
+      // console log: "You lose! Scissors beats Paper."
+      console.log("You lose! Scissors beats Paper.")
+      // increment computerScore variable by one
+      computerScore++;
+    // else if: humanChoice equals paper and computerChoice equals rock
+    } else if ((humanChoice === "paper") && (computerChoice === "rock")) {
+      // console log: "You win! Paper beats Rock."
+      console.log("You win! Paper beats Rock.")
+      // increment humanScore variable by one
+      humanScore++;
+  
+    // else if: humanChoice equals scissors and computerChoice equals rock
+    } else if ((humanChoice === "scissors") && (computerChoice === "rock")) {
+      // console log: "You lose! Rock beats Scissors."
+      console.log("You lose! Rock beats Scissors.");
+      // increment computerScore variable by one
+      computerScore++;
+  
+    // else if: humanChoice equals scissors and computerChoice equals paper
+    } else if ((humanChoice === "scissors") && (computerChoice === "paper")) {
+      // console log: "You win! Scissors beats Paper."
+      console.log("You win! Scissors beats Paper.");
+      // increment humanScore variable by one
+      humanScore++;
+  
+    // else: console log: "It's a tie";
+    } else {
+      console.log("It's a tie.");
+    }
+  }
 
   // Add a loop where playRound function is called 5 times
-  // Show the scores after each round (via console.log)
+  for (let i = 0; i < 5; i++) {
+    // Call getHumanChoice function and assign to humanSelection
+    humanSelection = getHumanChoice();
+
+    // optional: end loop if humanSelection is undefined
+    if (humanSelection == undefined) {
+      console.warn("Your Selection is undefined. Ending game.");
+      break;
+    }
+
+    // Call getComputerChoice function and assign to computerSelection
+    computerSelection = getComputerChoice();
+
+    // Display both human and computer selections
+    console.log("Human Selection: " + humanSelection);
+    console.log("Computer score: " + computerSelection);
+    
+    // Call playRound function here!
+    playRound(humanSelection, computerSelection);
+
+    // Show the scores after each round (via console.log)
+    console.log("Human score: " + humanScore);
+    console.log("Computer score: " + computerScore);
+  }
 
   // condition: if humanScore > computerScore, console.log: "You won!"
+  if (humanScore > computerScore) {
+    console.log("You won the Game!".toUpperCase());
 
-    // else if: computerScore > humanScore, console.log: "The computer won."
+  // else if: computerScore > humanScore, console.log: "The computer won."
+  } else if (computerScore > humanScore) {
+    console.log("The computer won.".toUpperCase());
 
     // else: console.log: "It's a tie."
+  } else {
+    console.log("Game ended in a tie.".toUpperCase());
+  }
 
 }
